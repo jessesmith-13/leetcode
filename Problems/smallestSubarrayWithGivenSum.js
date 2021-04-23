eca7c571e786ac25a7c2b20181618380013099d7
Jesse <jesseholden713@gmail.com>
2021-04-23 13:46:00 -0700

    Compelted LongestSubstringWithKDistinct

--- Problems/smallestSubarrayWithGivenSum.js
+++ Problems/smallestSubarrayWithGivenSum.js
@@ -1,25 +1,44 @@
-function smallest_subarray_with_given_sum(s, arr) {
-  let windowSum = 0,
-    minLength = Infinity,
-    windowStart = 0;
+// Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous 
+// subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
 
-  for (windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-    windowSum += arr[windowEnd]; // add the next element
-    // shrink the window as small as possible until the 'window_sum' is smaller than 's'
+// Example 1:
+// Input: [2, 1, 5, 2, 3, 2], S=7 
+// Output: 2
+// Explanation: The smallest subarray with a sum greater than or equal to '7' is [5, 2].
+
+// Example 2:
+// Input: [2, 1, 5, 2, 8], S=7 
+// Output: 1
+// Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+
+// Example 3:
+// Input: [3, 4, 1, 1, 6], S=8 
+// Output: 3
+// Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] 
+// or [1, 1, 6].
+
+function smallest_subarray_with_given_sum(s, arr) {
+  let minLength = Infinity,
+    windowStart = 0,
+    windowSum = 0;
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+
     while (windowSum >= s) {
       minLength = Math.min(minLength, windowEnd - windowStart + 1);
       windowSum -= arr[windowStart];
-      windowStart += 1;
+      windowStart++;
     }
   }
 
   if (minLength === Infinity) {
     return 0;
   }
+
   return minLength;
 }
 
 
 console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])}`);
-console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])}`);
-console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])}`);
+// console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])}`);
+// console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])}`);
